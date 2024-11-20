95c1b84fe807a9651bec7f4f396f68c88f7426cb
egor <TcvEg@DESKTOP-96TVH01>
2024-11-20 22:18:58 +0300

    Сделал класс ReportSales(Отчет по продажам), добавил метод, который считает общую выручку по всем фильмам.

---
+++ вторая л/ReportSales.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace вторая_л
+{
+    public class ReportSales
+    {
+        private List<SalesRecords> salesRecords;
+      
+        public ReportSales()
+        {
+            salesRecords = new List<SalesRecords>();
+        }
+
+        public void AddSale(SalesRecords record)
+        {
+            salesRecords.Add(record);
+        }
+
+        public void PrintReportAll()
+        {
+            double totalRevenue = 0;
+            foreach (var record in salesRecords)
+            {
+                var film = record.GetFilm();
+
+                Console.WriteLine($"День: {record.GetDay()}, Название фильма: {film.GetName()}, Жанр: {film.GetGenre()}, Цена: {film.GetPrice()}, Количество: {film.GetQuantity()}, Выручка с фильма: {film.GetPrice() * film.GetQuantity()}");
+                totalRevenue += film.GetQuantity() * film.GetPrice();
+            }
+            Console.WriteLine($"Общая выручка: {totalRevenue}");
+        }
+
+
+    }
+}
