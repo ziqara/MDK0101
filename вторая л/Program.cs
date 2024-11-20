401db0a2e2ca9788e88d673f6f39639113ad365c
egor <TcvEg@DESKTOP-96TVH01>
2024-11-20 22:07:57 +0300

    Cделал класс SalesRecords(Записи о продажах) с соответствующими методами.

--- вторая л/Program.cs
+++ вторая л/Program.cs
@@ -13,62 +13,7 @@ namespace вторая_л
         static void Main(string[] args)
         {
 
-            List<Film> films = new List<Film>()
-            {
-                new Film("Побег из шоушенка", Genre.Драма, 150),
-                new Film("Суперперцы", Genre.Комедия, 300),
-                new Film("Прочь", Genre.Ужасы, 50),
-                new Film("Начало", Genre.Фантастика, 200),
-                new Film("Индиана Джонс", Genre.Приключения, 400),
-                new Film("Побег из шоушенка", Genre.Боевик, 230)
-            };
-
-            Dictionary<Genre, (double totalSales, double totalRevenue)> salesRepost = new Dictionary<Genre, (double totalSales, double totalRevenue)>();
-            foreach (var genre in Enum.GetValues(typeof(Genre)))
-            {
-                salesRepost[(Genre)genre] = (0, 0);
-            }
-
-
-            int[,] fixedSales = new int[,]
-          {
-                { 3, 2, 1 },
-                { 2, 3, 2 },
-                { 1, 1, 0 },
-                { 0, 2, 1 },
-                { 1, 1, 1 }
-          };
-
-            for (int day = 0; day < 3; day++)
-            {
-                foreach (var filmss in films)
-                {
-                    int genreIndex = (int)filmss.Genre;
-                    if (genreIndex < fixedSales.GetLength(0))
-                    {
-                        int saleCount = fixedSales(genreIndex, day);
-                        salesRepost[films.Genre].totalSales += saleCount;
-                        salesRepost[films.Genre].totalRevenue += films.Price;
-                    }
-                    int salesCount = fixedSales[genreIndex, day];
-
-                    var currentSalesData = salesRepost[filmss.Genre];
-
-                    currentSalesData.totalSales += salesCount;
-                    currentSalesData.totalRevenue += salesCount * filmss.Price;
-
-                    salesRepost[filmss.Genre] = currentSalesData;
-                }
-            }
-
-            Console.WriteLine("Отчёт по продажам фильмов по жанрам:");
-            foreach (var report in salesRepost)
-            {
-                Console.WriteLine($"Жанр: {report.Key}");
-                Console.WriteLine($"Общее количество продаж: {report.Value.totalSales}");
-                Console.WriteLine($"Общая выручка: {report.Value.totalRevenue}");
-                Console.WriteLine();
-            }
+           
         }
     }
 }
---
+++ вторая л/SalesRecords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace вторая_л
+{
+    public class SalesRecords
+    {
+
+        private int day_;
+        private Film film_;
+
+        public SalesRecords(int day, Film film)
+        {
+            day_ = day;
+            film_ = film;
+        }
+
+        public int GetDay()
+        {
+            return day_;
+        }
+
+        public Film GetFilm()
+        {
+            return film_;
+        }
+    }
+}
+
