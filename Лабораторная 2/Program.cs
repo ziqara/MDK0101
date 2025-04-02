4f40825de7b0a61f3abff3c25094c9f72741cec4
ziqara <165387888+ziqara@users.noreply.github.com>
2025-04-03 00:57:21 +0300

    Реализовал методы в Program.cs

--- Лабораторная 2/Program.cs
+++ Лабораторная 2/Program.cs
@@ -1,52 +1,65 @@
-﻿using Microsoft.Win32.SafeHandles;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using вторая_л;
 
-
-namespace вторая_л
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        ReportSales report = new ReportSales();
+        report.AddSale(new SalesRecords(1, new Film("Зеленая миля", 1500, 12, Genre.Драма)));
+        report.AddSale(new SalesRecords(1, new Film("Работа с автостопом", 1700, 6, Genre.Драма)));
+
+        report.AddSale(new SalesRecords(2, new Film("Суперперцы", 2000, 8, Genre.Комедия)));
+        report.AddSale(new SalesRecords(2, new Film("Служебный роман", 1000, 5, Genre.Комедия)));
+
+        report.AddSale(new SalesRecords(3, new Film("Сияние", 350, 13, Genre.Ужасы)));
+        report.AddSale(new SalesRecords(3, new Film("Прочь", 500, 9, Genre.Ужасы)));
+
+        report.AddSale(new SalesRecords(4, new Film("Начало", 700, 11, Genre.Фантастика)));
+        report.AddSale(new SalesRecords(4, new Film("Матрица", 525, 1, Genre.Фантастика)));
+
+        report.AddSale(new SalesRecords(5, new Film("Индиана Джонс: В поисках утраченного ковчега", 230, 10, Genre.Приключения)));
+        report.AddSale(new SalesRecords(5, new Film("Пираты Карибского моря: Проклятие Черной жемчужины", 725, 21, Genre.Приключения)));
+
+        report.AddSale(new SalesRecords(6, new Film("Мстители", 125, 17, Genre.Боевик)));
+        report.AddSale(new SalesRecords(6, new Film("Джон Уик", 2300, 41, Genre.Боевик)));
+
+        Console.WriteLine("Выберите вид отчёта (1 - весь, 2 - по жанрам): ");
+        if (!int.TryParse(Console.ReadLine(), out int reportType))
         {
-            ReportSales report = new ReportSales();
-            report.AddSale(new SalesRecords(1, new Film("Зеленая миля", 1500, 12, Genre.Драма)));
-            report.AddSale(new SalesRecords(1, new Film("Работа с автостопом", 1700, 6, Genre.Драма)));
+            Console.WriteLine("Некорректный ввод");
+            return;
+        }
 
-            report.AddSale(new SalesRecords(2, new Film("Суперперцы", 2000, 8, Genre.Комедия)));
-            report.AddSale(new SalesRecords(2, new Film("Служебный роман", 1000, 5, Genre.Комедия)));
+        double totalRevenue;
+        List<string> reportLines = new List<string>();
 
-            report.AddSale(new SalesRecords(3, new Film("Сияние", 350, 13, Genre.Ужасы)));
-            report.AddSale(new SalesRecords(3, new Film("Прочь", 500, 9, Genre.Ужасы)));
+        switch (reportType)
+        {
+            case 1:
+                reportLines = report.GetReportAll(out totalRevenue);
+                break;
 
-            report.AddSale(new SalesRecords(4, new Film("Начало", 700, 11, Genre.Фантастика)));
-            report.AddSale(new SalesRecords(4, new Film("Матрица", 525, 1, Genre.Фантастика)));
+            case 2:
+                Console.WriteLine("Введите жанр: ");
+                string genreInput = Console.ReadLine();
+                reportLines = report.GetReportPerGenre(genreInput, out totalRevenue);
+                break;
 
-            report.AddSale(new SalesRecords(5, new Film("Индиана Джонс: В поисках утраченного ковчега", 230, 10, Genre.Приключения)));
-            report.AddSale(new SalesRecords(5, new Film("Пираты Карибского моря: Проклятие Черной жемчужины", 725, 21, Genre.Приключения)));
-
-            report.AddSale(new SalesRecords(6, new Film("Мстители", 125, 17, Genre.Боевик)));
-            report.AddSale(new SalesRecords(6, new Film("Джон Уик", 2300, 41, Genre.Боевик)));
-
-
-            int reportType;
-            Console.WriteLine("Выберите вид отчёта (1 - весь, 2 - по жанрам): ");
-            reportType = Convert.ToInt32(Console.ReadLine());
-            if (reportType == 1)
-            {
-                report.PrintReportAll();
-            }
-            else if (reportType == 2)
-            {
-                report.PrintReportPerGenre();
-            }
-            else
-            {
+            default:
                 Console.WriteLine("Введено неверное значение");
-            }
+                return;
+        }
+
+        if (reportLines.Count > 0)
+        {
+            reportLines.ForEach(Console.WriteLine);
+            Console.WriteLine($"Общая выручка: {totalRevenue}");
+        }
+        else
+        {
+            Console.WriteLine("Выручка по жанру нулевая/Данного жанра не существует");
         }
     }
 }
--- Лабораторная 2/ReportSales.cs
+++ Лабораторная 2/ReportSales.cs
@@ -1,62 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using вторая_л;
 
-namespace вторая_л
+public class ReportSales
 {
-    public class ReportSales
+    private List<SalesRecords> salesRecords;
+
+    public ReportSales()
     {
-        private List<SalesRecords> salesRecords;
+        salesRecords = new List<SalesRecords>();
+    }
 
-        public ReportSales()
+    public void AddSale(SalesRecords record)
+    {
+        salesRecords.Add(record);
+    }
+
+    public List<string> GetReportAll(out double totalRevenue)
+    {
+        totalRevenue = 0;
+        List<string> reportLines = new List<string>();
+
+        foreach (var record in salesRecords)
         {
-            salesRecords = new List<SalesRecords>();
+            var film = record.GetFilm();
+            double revenue = film.GetPrice() * film.GetQuantity();
+            reportLines.Add($"День: {record.GetDay()}, Название фильма: {film.GetName()}, Жанр: {film.GetGenre()}, Цена: {film.GetPrice()}, Количество: {film.GetQuantity()}, Выручка с фильма: {revenue}");
+            totalRevenue += revenue;
         }
 
-        public void AddSale(SalesRecords record)
+        return reportLines;
+    }
+
+    public List<string> GetReportPerGenre(string genreInput, out double totalRevenue)
+    {
+        totalRevenue = 0;
+        List<string> reportLines = new List<string>();
+
+        foreach (var record in salesRecords)
         {
-            salesRecords.Add(record);
+            var film = record.GetFilm();
+            if (film.GetGenre().ToString() == genreInput)
+            {
+                double revenue = film.GetPrice() * film.GetQuantity();
+                reportLines.Add($"День: {record.GetDay()}, Название фильма: {film.GetName()}, Жанр: {film.GetGenre()}, Цена: {film.GetPrice()}, Количество: {film.GetQuantity()}, Выручка с фильма: {revenue}");
+                totalRevenue += revenue;
+            }
         }
 
-        public void PrintReportAll() 
-        {
-            double totalRevenue = 0;
-            foreach (var record in salesRecords)
-            {
-                var film = record.GetFilm();
-
-                Console.WriteLine($"День: {record.GetDay()}, Название фильма: {film.GetName()}, Жанр: {film.GetGenre()}, Цена: {film.GetPrice()}, Количество: {film.GetQuantity()}, Выручка с фильма: {film.GetPrice() * film.GetQuantity()}");
-                totalRevenue += film.GetQuantity() * film.GetPrice();
-            }
-            Console.WriteLine($"Общая выручка: {totalRevenue}");
-        }
-
-        public void PrintReportPerGenre()
-        {
-            string genre;
-            Console.WriteLine("Введите жанр: ");
-            genre = Console.ReadLine();
-
-            double totalRevenue = 0;
-            foreach (var record in salesRecords)
-            {
-                var film = record.GetFilm();
-                if (genre == Convert.ToString(film.GetGenre()))
-                {
-                    Console.WriteLine($"День: {record.GetDay()}, Название фильма: {film.GetName()}, Жанр: {film.GetGenre()}, Цена: {film.GetPrice()}, Количество: {film.GetQuantity()}, Выручка с фильма: {film.GetPrice() * film.GetQuantity()}");
-                    totalRevenue += film.GetQuantity() * film.GetPrice();
-                }
-            }
-            if (totalRevenue != 0)
-            {
-                Console.WriteLine($"Общая выручка: {totalRevenue}");
-            }
-            if (totalRevenue == 0)
-            {
-                Console.WriteLine("Выручка по жанру нулевая/Данного жанра не существует");
-            }
-        }
+        return reportLines;
     }
 }
